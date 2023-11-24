54a45bcf1f45433de0a78c8e7f5183d9add13d05
Deepak-coder <deepakpwian@gmail.com>
2023-11-25 00:03:13 +0530

    n

--- 2.c
+++ 2.c
@@ -10,22 +10,22 @@
 
 // }
 
-// #include<stdio.h>
-// int main()
-// {
-//     int a;
-//     printf("Enter a positive number:");
-//     scanf("%d" ,&a);
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a positive number:");
+    scanf("%d" ,&a);
 
-//     if (a % 5  == 0)
-//     {
-//         printf("the entered number is divisible by 5");
+    if (a % 5  == 0)
+    {
+        printf("the entered number is divisible by 5");
 
-//     }
-//     else
-//     {
-//         printf("the enterd number is not divisible by 5");
-//     }
+    }
+    else
+    {
+        printf("the enterd number is not divisible by 5");
+    }
 
-// return 0;
-// }
+return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,19 @@
-o.h>
-// int main()
-// {
-//     int a ,b;
-//     printf("Enter the 1st number\n");
-//     scanf("%d",&a);
-//     printf("Enter the 2nd number\n");
-//     scanf("%d",&b);
-//     printf("the difference of the numbers is %d \n", a-b);
+stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a positive number:");
+    scanf("%d" ,&a);
 
-// }
+    if (a % 5  == 0)
+    {
+        printf("the entered number is divisible by 5");
+
+    }
+    else
+    {
+        printf("the enterd number is not divisible by 5");
+    }
+
+return 0;
+}
