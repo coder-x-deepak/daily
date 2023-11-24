479e915374920898dc9997471cf494141c745e3a
Deepak-coder <deepakpwian@gmail.com>
2023-11-24 23:50:02 +0530

    add nw

---
+++ 2.c
@@ -0,0 +1,31 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a ,b;
+//     printf("Enter the 1st number\n");
+//     scanf("%d",&a);
+//     printf("Enter the 2nd number\n");
+//     scanf("%d",&b);
+//     printf("the difference of the numbers is %d \n", a-b);
+
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("Enter a positive number:");
+//     scanf("%d" ,&a);
+
+//     if (a % 5  == 0)
+//     {
+//         printf("the entered number is divisible by 5");
+
+//     }
+//     else
+//     {
+//         printf("the enterd number is not divisible by 5");
+//     }
+
+// return 0;
+// }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+o.h>
+// int main()
+// {
+//     int a ,b;
+//     printf("Enter the 1st number\n");
+//     scanf("%d",&a);
+//     printf("Enter the 2nd number\n");
+//     scanf("%d",&b);
+//     printf("the difference of the numbers is %d \n", a-b);
+
+// }
