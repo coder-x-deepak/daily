beeb9f8615c7466aeaa73286dcc01bdf7e1f352d
Deepak-coder <deepakpwian@gmail.com>
2023-12-16 07:16:08 +0530

    compound intrest

---
+++ compound_intrest.c
@@ -0,0 +1,10 @@
+// #include <stdio.h>
+// #include <math.h>
+// int main()
+// {
+//     float p,r,t;
+//     scanf("%f%f%f",&p,&r,&t);
+//     float compound_intrest = p*pow(1+r/100,t) - p ;
+//     printf("%f",compound_intrest);
+//     return 0;
+// }
